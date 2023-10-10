b0f440729d75db919f66417a43ae714961fc56dd
sloggo <70816908+sloggo@users.noreply.github.com>
2023-10-10 14:25:55 +0100

    Begin UI by initialising a blank main screen

--- src/questionGame.java
+++ src/questionGame.java
@@ -1,8 +1,14 @@
+import javax.swing.*;
+
 public class questionGame {
     public static void main(String[] args) throws Exception {
+        initMenu();
     }
 
     public static void initMenu(){
-        
+        JFrame frame = new JFrame("Question Game");
+        frame.setExtendedState(JFrame.MAXIMIZED_BOTH); 
+        frame.setVisible(true);
+
     }
 }
