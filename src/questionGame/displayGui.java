d7de319b2889def2dee674957398b4cd0a1b764e
sloggo <70816908+sloggo@users.noreply.github.com>
2023-10-10 15:26:58 +0100

    Align items in the GUI using border layout and panels

--- src/questionGame/displayGui.java
+++ src/questionGame/displayGui.java
@@ -1,6 +1,9 @@
 package questionGame;
+import java.awt.BorderLayout;
+
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class displayGui  extends JFrame{
@@ -12,9 +15,16 @@ public class displayGui  extends JFrame{
         JPanel topPanel = new JPanel();
         JPanel bottomPanel = new JPanel();
 
-        JButton button = new JButton("Button");
-        topPanel.add(button);
+        JLabel title = new JLabel("Welcome to Question Game!");
+        title.setVerticalTextPosition(JLabel.BOTTOM);
+        title.setHorizontalTextPosition(JLabel.CENTER);
 
-        frame.getContentPane().add(topPanel);
+        JButton button = new JButton("Button");
+        topPanel.add(title);
+        bottomPanel.add(button);
+
+
+        frame.getContentPane().add(topPanel, BorderLayout.NORTH);
+        frame.getContentPane().add(bottomPanel, BorderLayout.SOUTH);
     }
 }
