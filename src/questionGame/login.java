19cd3e5edecd08c3aadcc61c6f3f0b74675d5ea5
Patrickwalsh33 <patrickwalsh33@outlook.com>
2023-10-11 19:57:51 +0100

    Scanner framework

--- src/questionGame/login.java
+++ src/questionGame/login.java
@@ -1,7 +1,15 @@
 package questionGame;
-
+import java.util.Scanner;
 public class login {
     public static void main(String[] args){
+        menu();
+        
+        
+    }
+    public static void menu(){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 0 to sign up or enter 1 to Log in");
+        int menuchoice = scanner.nextInt();
         
     }
 }
