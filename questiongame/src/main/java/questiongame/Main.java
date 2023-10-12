32f2cd02f18a3aec6dc906a253279abc7714fec1
sloggo <70816908+sloggo@users.noreply.github.com>
2023-10-12 11:19:50 +0100

    Connect to database and collection of users

--- questiongame/src/main/java/questiongame/Main.java
+++ questiongame/src/main/java/questiongame/Main.java
@@ -1,7 +1,18 @@
 package questiongame;
+import static com.mongodb.client.model.Filters.eq;
+import org.bson.Document;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        database.connect();
+        database databaseController = new database();
+
+        MongoDatabase mongoDB = databaseController.mongoClient.getDatabase("questionGame");
+        MongoCollection<Document> usersCollection = mongoDB.getCollection("users");
+
+        System.out.println(usersCollection);
     }
 }
--- questiongame/src/main/java/questiongame/database.java
+++ questiongame/src/main/java/questiongame/database.java
@@ -7,11 +7,7 @@ import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
 public class database {
-    
-    public static void connect(){
-        String uri = "mongodb+srv://sloggo:sloggo@questiongame.ivpo6ff.mongodb.net/?retryWrites=true&w=majority";
-        try (MongoClient mongoClient = MongoClients.create(uri)) {
-            System.out.println("sauce");
-        }
-    }
+    String uri = "mongodb+srv://sloggo:sloggo@questiongame.ivpo6ff.mongodb.net/?retryWrites=true&w=majority";
+    MongoClient mongoClient = MongoClients.create(uri);
+
 }
