9dd2ad59395c472ed2ea95acc3ead37f44c9b07c
Patrickwalsh33 <patrickwalsh33@outlook.com>
2023-10-16 20:53:42 +0100

    question constructor created

--- questiongame/src/main/java/questiongame/question.java
+++ questiongame/src/main/java/questiongame/question.java
@@ -6,4 +6,12 @@ public class question {
     String question;
     char answer;
     String[] options;
+
+    public question(topic topic, difficulty difficulty, String question, char answer, String[] options){
+        this.topic = topic;
+        this.difficulty = difficulty;
+        this.question = question;
+        this.answer = answer;
+        this.options = options;
+    }
 }
