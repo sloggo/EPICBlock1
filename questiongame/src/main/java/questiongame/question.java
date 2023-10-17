a9b436e94daab0e086e9cab2da006a6fae7749c8
sloggo <70816908+sloggo@users.noreply.github.com>
2023-10-17 15:11:57 +0100

    Add sorting questions algorithm

--- questiongame/src/main/java/questiongame/question.java
+++ questiongame/src/main/java/questiongame/question.java
@@ -1,5 +1,7 @@
 package questiongame;
 
+import java.util.Arrays;
+
 public class Question {
     topic topic;
     difficulty difficulty;
@@ -18,5 +20,21 @@ public class Question {
     Question[] Questions = {
         new Question(topic.DISCMATHS, difficulty.NOVICE, "test", 'a', new String[]{"opta", "optb"})
     };
+
+    public Question[] getAllQuestions(){
+        return Questions;
+    }
+
+    public Question[] fetchSpecificQuestions(topic topic, difficulty difficulty){
+        Question[] sortedQuestions = {};
+        for(int i=0; i<Questions.length; i++){
+            if(Questions[i].topic == topic && Questions[i].difficulty == difficulty){
+                sortedQuestions = Arrays.copyOf(sortedQuestions, sortedQuestions.length + 1);
+                sortedQuestions[sortedQuestions.length - 1] = Questions[i];
+            } 
+        }
+        System.out.println(sortedQuestions.toString())
+        return sortedQuestions;
+    }
 }
 
