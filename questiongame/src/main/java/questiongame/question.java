0f14f1719d86bbddeb00ed7f9c88237cf1f3e5c6
sloggo <70816908+sloggo@users.noreply.github.com>
2023-10-17 15:22:46 +0100

    Fix errors in files

--- questiongame/src/main/java/questiongame/question.java
+++ questiongame/src/main/java/questiongame/question.java
@@ -1,17 +1,40 @@
 package questiongame;
 
-public class question {
+import java.util.Arrays;
+
+public class Question {
     topic topic;
     difficulty difficulty;
     String question;
     char answer;
     String[] options;
 
-    public question(topic topic, difficulty difficulty, String question, char answer, String[] options){
+    public Question(topic topic, difficulty difficulty, String question, char answer, String[] options){
         this.topic = topic;
         this.difficulty = difficulty;
         this.question = question;
         this.answer = answer;
         this.options = options;
     }
+
+    Question[] Questions = {
+        new Question(topic.DISCMATHS, difficulty.NOVICE, "test", 'a', new String[]{"opta", "optb"})
+    };
+
+    public Question[] getAllQuestions(){
+        return Questions;
+    }
+
+    public Question[] fetchSpecificQuestions(topic topic, difficulty difficulty){
+        Question[] sortedQuestions = {};
+        for(int i=0; i<Questions.length; i++){
+            if(Questions[i].topic == topic && Questions[i].difficulty == difficulty){
+                sortedQuestions = Arrays.copyOf(sortedQuestions, sortedQuestions.length + 1);
+                sortedQuestions[sortedQuestions.length - 1] = Questions[i];
+            } 
+        }
+        System.out.println(sortedQuestions.toString());
+        return sortedQuestions;
+    }
 }
+
