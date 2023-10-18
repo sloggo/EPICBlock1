0b2bfb70970c83a486003a40e345f5100959ee80
Patrickwalsh33 <patrickwalsh33@outlook.com>
2023-10-18 16:20:42 +0100

    Select random question for randomMode

--- questiongame/src/main/java/questiongame/modeController.java
+++ questiongame/src/main/java/questiongame/modeController.java
@@ -2,6 +2,8 @@ package questiongame;
 import java.lang.reflect.Array;
 import java.util.Arrays;
 import java.util.Scanner;
+// importing random package
+import java.util.Random;
 
 public class modeController {
 
@@ -64,6 +66,17 @@ public class modeController {
         for(Question q : sortedQ){ // loop through selected questions
             printQuestion(q);
         }
+    }
+
+    public static void randomMode(Question[] questions){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to Random Mode!");
+        int questionLength = questions.length;
+        Random random = new Random();
+        int randomIndex = random.nextInt(questionLength);
+        Question randomQ = questions[randomIndex];
+
+        System.out.println(randomQ.question);
     }
 
 
