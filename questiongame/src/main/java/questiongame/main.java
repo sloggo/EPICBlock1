52de95e5c9e2e00ddebfdb21f89e0e4045789e4b
sloggo <70816908+sloggo@users.noreply.github.com>
2023-10-18 15:26:43 +0100

    Put modes inside their own controller file

--- questiongame/src/main/java/questiongame/main.java
+++ questiongame/src/main/java/questiongame/main.java
@@ -45,62 +45,10 @@ public class Main {
             new Question(questiongame.topic.COMPORG, questiongame.difficulty.EXPERT, "-\tWhich of the below are NOT found in the original Von Neumann machine", 'a', new String[]{"opta", "optb"}),
             new Question(questiongame.topic.COMPORG, questiongame.difficulty.EXPERT, "Convert the hexadecimal number 2A3F to binary (ans = 1010001001111111)", 'a', new String[]{"opta", "optb"}),
         };
-
         Document user = accountController.menu();
 
-        difficultyMode(Questions);
+        modeController.difficultyMode(Questions);
     }  
 
-
-    public static Question[] fetchSpecificQuestions(topic topic, difficulty difficulty, Question[] questions){
-        Question[] sortedQuestions = {};
-        for(int i=0; i<questions.length; i++){
-            if(topic != null && difficulty != null){ // if searching by both
-                if(questions[i].topic == topic && questions[i].difficulty == difficulty){
-                    sortedQuestions = Arrays.copyOf(sortedQuestions, sortedQuestions.length + 1);
-                    sortedQuestions[sortedQuestions.length - 1] = questions[i];
-                } 
-            } else if(topic == null){ // if searching by difficulty only
-                if(questions[i].difficulty == difficulty){
-                    sortedQuestions = Arrays.copyOf(sortedQuestions, sortedQuestions.length + 1);
-                    sortedQuestions[sortedQuestions.length - 1] = questions[i];
-                } 
-            } else if(difficulty == null){ // if searching by topic only
-                if(questions[i].topic == topic){
-                    sortedQuestions = Arrays.copyOf(sortedQuestions, sortedQuestions.length + 1);
-                    sortedQuestions[sortedQuestions.length - 1] = questions[i];
-                } 
-            }
-            
-        }
-        System.out.println(sortedQuestions.toString());
-        return sortedQuestions;
-    }
-
-    public static void difficultyMode(Question[] questions){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Welcome to difficulty mode!");
-
-        System.out.println("1 - Novice | 2 - Intermediate | 3 - Expert");
-
-        int input = scanner.nextInt();
-        questiongame.difficulty difficultySelection = null;
-        switch(input){ // translate input to a difficulty selection
-            case(1):
-                difficultySelection = questiongame.difficulty.NOVICE; 
-                break;
-            case(2):
-                difficultySelection = questiongame.difficulty.INTERMEDIATE;
-                break;
-            case(3):
-                difficultySelection = questiongame.difficulty.EXPERT;
-        }
-
-        Question[] sortedQ = fetchSpecificQuestions(null, difficultySelection, questions);
-
-        for(Question q : sortedQ){ // loop through selected questions
-            System.out.println(q.question);
-        }
-    }
 
 }
---
+++ questiongame/src/main/java/questiongame/modeController.java
@@ -0,0 +1,57 @@
+package questiongame;
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class modeController {
+
+    public static void difficultyMode(Question[] questions){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Welcome to difficulty mode!");
+
+        System.out.println("1 - Novice | 2 - Intermediate | 3 - Expert");
+
+        int input = scanner.nextInt();
+        questiongame.difficulty difficultySelection = null;
+        switch(input){ // translate input to a difficulty selection
+            case(1):
+                difficultySelection = questiongame.difficulty.NOVICE; 
+                break;
+            case(2):
+                difficultySelection = questiongame.difficulty.INTERMEDIATE;
+                break;
+            case(3):
+                difficultySelection = questiongame.difficulty.EXPERT;
+        }
+
+        Question[] sortedQ = fetchSpecificQuestions(null, difficultySelection, questions);
+
+        for(Question q : sortedQ){ // loop through selected questions
+            System.out.println(q.question);
+        }
+    }
+
+    public static Question[] fetchSpecificQuestions(topic topic, difficulty difficulty, Question[] questions){
+            Question[] sortedQuestions = {};
+            for(int i=0; i<questions.length; i++){
+                if(topic != null && difficulty != null){ // if searching by both
+                    if(questions[i].topic == topic && questions[i].difficulty == difficulty){
+                        sortedQuestions = Arrays.copyOf(sortedQuestions, sortedQuestions.length + 1);
+                        sortedQuestions[sortedQuestions.length - 1] = questions[i];
+                    } 
+                } else if(topic == null){ // if searching by difficulty only
+                    if(questions[i].difficulty == difficulty){
+                        sortedQuestions = Arrays.copyOf(sortedQuestions, sortedQuestions.length + 1);
+                        sortedQuestions[sortedQuestions.length - 1] = questions[i];
+                    } 
+                } else if(difficulty == null){ // if searching by topic only
+                    if(questions[i].topic == topic){
+                        sortedQuestions = Arrays.copyOf(sortedQuestions, sortedQuestions.length + 1);
+                        sortedQuestions[sortedQuestions.length - 1] = questions[i];
+                    } 
+                }
+                
+            }
+            System.out.println(sortedQuestions.toString());
+            return sortedQuestions;
+        }
+}
