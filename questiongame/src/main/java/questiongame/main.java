bdeac8111939ea8371f02bb628a620cccf404aac
Patrickwalsh33 <patrickwalsh33@outlook.com>
2023-11-01 14:37:45 +0000

    questions updated

--- questiongame/src/main/java/questiongame/main.java
+++ questiongame/src/main/java/questiongame/main.java
@@ -24,21 +24,24 @@ public class Main {
                     "Who is the knight, who is the knave, and who is the spy?\r\n" + //
                     "", "4", new String[]{"A: Knight, B:Knave, C:Spy", "A:Spy, B:Knight, C:Knave", "A:Knave, B:Spy, C:Knight", "A:Knight, B:Spy, C:Knave"}),
             //need to insert matrix image in question on next line
-            new Question(questiongame.topic.DISCMATHS, questiongame.difficulty.EXPERT, "A system can take one of three states 1, 2, or 3. The system will change its status each second. The probability that a system will change from one state to another is given using the following adjacency matrix: ", "1", new String[]{"opta", "optb", "optc", "optd"}),
+            new Question(questiongame.topic.DISCMATHS, questiongame.difficulty.EXPERT, "Find B when [ 3,7 \\n" + //
+                    " 9,3] *B = [ 2,8 \\n" + //
+                    " 7,10] ", "1", new String[]{"opta", "optb", "optc", "optd"}),
             //Computer Science
             //Novice
-            new Question(questiongame.topic.COMPSCIFOUND, questiongame.difficulty.NOVICE, "-\tExplain what is meant by a Backus Naur Form", "1", new String[]{"opta", "optb", "optc", "optd"}),
+            new Question(questiongame.topic.COMPSCIFOUND, questiongame.difficulty.NOVICE, "-\tWhich option best desribes Backus Naur Form", "2", new String[]{"Binary code written in a human readable form", "Defines the grammar for a programming language", "Defines the semantics of a program", "The infima of a lattice"}),
             new Question(questiongame.topic.COMPSCIFOUND, questiongame.difficulty.NOVICE, "-\tWhat does the term BDD stand for?", "4", new String[]{"Bernhard Description Diagram", "Braunt Design Diagram", "Backus Definition Diagram", "Boolean Decision Diagram"}),
             //Intermediate
-            new Question(questiongame.topic.COMPSCIFOUND, questiongame.difficulty.INTERMEDIATE, "-\tWhat are the suprema and infima of this set? A = {1,2,3,4}", "1", new String[]{"opta", "optb", "optc", "optd"}),
-            new Question(questiongame.topic.COMPSCIFOUND, questiongame.difficulty.INTERMEDIATE, "-\t Question", "1", new String[]{"opta", "optb", "optc", "optd"}),
+            new Question(questiongame.topic.COMPSCIFOUND, questiongame.difficulty.INTERMEDIATE, "-\tWhat is the supremum of this set? A = {1,2,3,4}", "1", new String[]{"4", "2", "3", "1"}),
+            new Question(questiongame.topic.COMPSCIFOUND, questiongame.difficulty.INTERMEDIATE, "-\t Which of the following is not a predicate", "1", new String[]{"Paris is the capital of France.", "P(x) = {x is a positive integer.}", "S(x, y) = {x is greater than the square of 
+            }", "R(x) = {x is a square number}"}),
             //Expert
-            new Question(questiongame.topic.COMPSCIFOUND, questiongame.difficulty.EXPERT, "-\tQuestion", "1", new String[]{"opta", "optb", "optc", "optd"}),
-            new Question(questiongame.topic.COMPSCIFOUND, questiongame.difficulty.EXPERT, "-\tWhich of the following is a predicate", "1", new String[]{"opta", "optb", "optc", "optd"}),
+            new Question(questiongame.topic.COMPSCIFOUND, questiongame.difficulty.EXPERT, "-\tWhat does ‘reductio ad absurdum’ mean", "2", new String[]{"Proof by induction", "Proof by contradiction", "Direct Proof", "Proof by cases"}),
+            new Question(questiongame.topic.COMPSCIFOUND, questiongame.difficulty.EXPERT, "-\tWhich of the following is an example of commutativity ", "3", new String[]{"A u (A n B) = A n B", "A u B = A'(A n B)", "A n B = B n A", "B u B = A n B"}),
             //Computer Organisation
             //Novice
             new Question(questiongame.topic.COMPORG, questiongame.difficulty.NOVICE, "-\tOut of registers and the cache in a computer, which of the two is located inside the CPU.", "2", new String[]{"Cache", "Registers", "Both", "Neither"}),
-            new Question(questiongame.topic.COMPORG, questiongame.difficulty.NOVICE, "-\tWhich of the below best describes the Von Neumann bottleneck", "1", new String[]{"opta", "optb", "optc", "optd"}),
+            new Question(questiongame.topic.COMPORG, questiongame.difficulty.NOVICE, "-\tWhich of the below best describes the Von Neumann bottleneck", "2", new String[]{"when the CPU can't handle large quantities of data", "limited bandwidth and simultaneous data transfer capabilities between a computer's central processing unit (CPU) and memory", "Having too much RAM on your computer", "Too much requests of the CPU, causing it to overload"}),
             //Intermediate
             new Question(questiongame.topic.COMPORG, questiongame.difficulty.INTERMEDIATE, "-\tConvert the binary number 1101 to decimal", "3", new String[]{"33", "3", "13", "53"}),
             new Question(questiongame.topic.COMPORG, questiongame.difficulty.INTERMEDIATE, "-\tWhich of the below are NOT found in the original Von Neumann machine ", "1", new String[]{"Audio Processor", "Memory", "Control Unit", "Arithmetic logic unit"}),
